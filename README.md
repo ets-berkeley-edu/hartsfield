d8f28dc3129d870158cd8bba801fc6a47316445a
Cesar Villalobos <cesarv.h@berkeley.edu>
2023-02-16 13:15:27 -0800

    ATL-1: Hello world. Initial install/setup

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # hartsfield
 UI component for retrieving DataHub archived files.
+
+
+## Installation
+
+* Install Python 3
+* Create your virtual environment (venv)
+* Install Node 16.19.0
+* Install dependencies
+
+
+### Front-end dependencies
+
+```
+npm install
+```
+
+### Run the app locally
+
+```
+npm run dev
+```
---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import Hartsfield from "./components/Hartsfield.vue";
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <Hartsfield msg="You did it!" />
+    </div>
+  </header>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+}
+</style>
