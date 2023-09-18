301136ff1fa635ca5fd6d141e894f586d21fceab
Greg Merritt <gmerritt@berkeley.edu>
2023-09-18 15:48:14 -0700

    initial commit

---
+++ src/router.ts
@@ -0,0 +1,32 @@
+import { createRouter, createWebHistory } from 'vue-router'
+const Fetchurl = () => import('./views/Fetchurl.vue')
+
+const routes = [
+  {
+    path: '/',
+    component: () => import('@/layouts/default/Default.vue'),
+    children: [
+      {
+        path: '',
+        name: 'Nostromo',
+        // Lazy-load components
+        component: () => import('@/views/Nostromo.vue'),
+      },
+    ],
+  },
+  {
+    component: Fetchurl,
+    path: '/fetchurl',
+    meta: {
+      title: 'Fetch URL',
+    }
+  },
+
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+})
+
+export default router
