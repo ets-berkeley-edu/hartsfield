bb21919a7f64803ea759e44905a46fc86762cf9a
Greg Merritt <gmerritt@berkeley.edu>
2023-12-15 14:07:20 -0800

    Emptying for merge

--- babel.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  presets: ["@vue/app", "@babel/preset-env"],
-  plugins: [["@babel/transform-runtime"]],
-};
--- src/views/Home.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <Login />
-</template>
-
-<script lang="ts" setup>
-  import Login from '@/views/Login.vue'
-</script>
