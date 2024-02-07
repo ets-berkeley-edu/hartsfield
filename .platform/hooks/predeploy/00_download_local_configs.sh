7662aac7756a508417c25176ff8c8ef14b8f5122
Greg Merritt <gmerritt@berkeley.edu>
2024-02-07 15:44:25 -0800

    undoing fake secrets try (from s3)

--- .platform/hooks/predeploy/00_download_local_configs.sh
+++ .platform/hooks/predeploy/00_download_local_configs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-PYTHONPATH='' aws secretsmanager get-secret-value --secret-id hartsfield-${EB_ENVIRONMENT}.py --query 'SecretString' --output text > config/production-local.py
+PYTHONPATH='' aws s3 cp s3://hartsfield-deploy-configs/hartsfield/${EB_ENVIRONMENT}.py config/production-local.py
 printf "\nEB_ENVIRONMENT = '${EB_ENVIRONMENT}'\n\n" >> config/production-local.py
 chown webapp config/production-local.py
 chmod 400 config/production-local.py
