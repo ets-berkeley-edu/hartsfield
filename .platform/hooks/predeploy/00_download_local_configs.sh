9554b2e0e5a70e6e2861fbfb6e42ffd86cb73e76
Greg Merritt <gmerritt@berkeley.edu>
2024-02-16 13:13:38 -0800

    Removing S3-specific config handling

--- .platform/hooks/predeploy/00_download_local_configs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-PYTHONPATH='' aws s3 cp s3://hartsfield-deploy-configs/hartsfield/${EB_ENVIRONMENT}.py config/production-local.py
-printf "\nEB_ENVIRONMENT = '${EB_ENVIRONMENT}'\n\n" >> config/production-local.py
-chown webapp config/production-local.py
-chmod 400 config/production-local.py
